1b7e4abd9047bc15742245239a44cd5856526aec
francis <dominiquefrancis42@gmail.com>
2025-10-19 11:31:36 +0800

    fix: vite config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        js: "js/index.html",
+        ts: "ts/index.html",
+        nodejs: "nodejs/index.html",
+        react: "react/index.html",
+      },
+    },
+  },
 });
